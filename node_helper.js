b9a5a6eef98533631ad8c9b7b6521827a0b8d47f
Abbondanzo <peter@abbondanzo.com>
2017-07-17 01:43:47 -0400

    Support for always-on and always-off switches

--- node_helper.js
+++ node_helper.js
@@ -1,90 +1,139 @@
 'use strict';
 
 /* Magic Mirror
- * Module: MMM-PIR-Sensor
- *
- * By Paul-Vincent Roll http://paulvincentroll.com
- * MIT Licensed.
- */
+* Module: MMM-PIR-Sensor
+*
+* By Paul-Vincent Roll http://paulvincentroll.com
+* MIT Licensed.
+*/
 
 const NodeHelper = require('node_helper');
 const Gpio = require('onoff').Gpio;
 const exec = require('child_process').exec;
 
 module.exports = NodeHelper.create({
-  start: function () {
-    this.started = false;
-  },
+    start: function () {
+        this.started = false;
+    },
 
-  activateMonitor: function () {
-    if (this.config.relayPIN != false) {
-      this.relay.writeSync(this.config.relayOnState);
-    }
-    else if (this.config.relayPIN == false){
-      // Check if hdmi output is already on
-      exec("/opt/vc/bin/tvservice -s").stdout.on('data', function(data) {
-        if (data.indexOf("0x120002") !== -1)
-          exec("/opt/vc/bin/tvservice --preferred && chvt 6 && chvt 7", null);
-      });
-    }
-  },
-
-  deactivateMonitor: function () {
-    if (this.config.relayPIN != false) {
-      this.relay.writeSync(this.config.relayOffState);
-    }
-    else if (this.config.relayPIN == false){
-      exec("/opt/vc/bin/tvservice -o", null);
-    }
-  },
-
-  // Subclass socketNotificationReceived received.
-  socketNotificationReceived: function(notification, payload) {
-    if (notification === 'CONFIG' && this.started == false) {
-      const self = this;
-      this.config = payload;
-
-      // Setup value which represent on and off
-      const valueOn = this.config.invertSensorValue ? 0 : 1;
-      const valueOff = this.config.invertSensorValue ? 1 : 0;
-
-      //Setup pins
-      this.pir = new Gpio(this.config.sensorPIN, 'in', 'both');
-      // exec("echo '" + this.config.sensorPIN.toString() + "' > /sys/class/gpio/export", null);
-      // exec("echo 'in' > /sys/class/gpio/gpio" + this.config.sensorPIN.toString() + "/direction", null);
-
-      if (this.config.relayPIN) {
-        this.relay = new Gpio(this.config.relayPIN, 'out');
-        this.relay.writeSync(this.config.relayOnState);
-        exec("/opt/vc/bin/tvservice --preferred && chvt 6 && chvt 7", null);
-      }
-
-      //Detected movement
-      this.pir.watch(function(err, value) {
-        if (value == valueOn) {
-          self.sendSocketNotification("USER_PRESENCE", true);
-          if (self.config.powerSaving){
-            clearTimeout(self.deactivateMonitorTimeout);
-            self.activateMonitor();
-          }
-        }
-        else if (value == valueOff) {
-          self.sendSocketNotification("USER_PRESENCE", false);
-          if (!self.config.powerSaving){
+    activateMonitor: function () {
+        // If always-off is enabled, keep monitor deactivated
+        if (this.alwaysOff.readSync() === this.config.alwaysOffState) {
             return;
-          }
-
-          self.deactivateMonitorTimeout = setTimeout(function() {
-            self.deactivateMonitor();
-          }, self.config.powerSavingDelay * 1000);
         }
-      });
+        // If relays are being used in place of HDMI
+        if (this.config.relayPin !== false) {
+            this.relay.writeSync(this.config.relayState);
+        }
+        else if (this.config.relayPin === false) {
+            // Check if hdmi output is already on
+            exec('/opt/vc/bin/tvservice -s').stdout.on('data', function(data) {
+                if (data.indexOf('0x120002') !== -1)
+                exec('/opt/vc/bin/tvservice --preferred && chvt 6 && chvt 7', null);
+            });
+        }
+    },
 
-      this.started = true;
+    deactivateMonitor: function () {
+        // If always-on is enabled, keep monitor activated
+        if (this.alwaysOn.readSync() === this.config.alwaysOnState) {
+            return;
+        }
+        // If relays are being used in place of HDMI
+        if (this.config.relayPin !== false) {
+            this.relay.writeSync((this.config.relayState + 1) % 2);
+        }
+        else if (this.config.relayPin === false) {
+            exec('/opt/vc/bin/tvservice -o', null);
+        }
+    },
 
-    } else if (notification === 'SCREEN_WAKEUP') {
-      this.activateMonitor();
+    // Subclass socketNotificationReceived received.
+    socketNotificationReceived: function (notification, payload) {
+        if (notification === 'CONFIG' && this.started == false) {
+            const self = this;
+            this.config = payload;
+
+            // Setup for relay pin
+            if (this.config.relayPin) {
+                this.relay = new Gpio(this.config.relayPin, 'out');
+                this.relay.writeSync(this.config.relayState);
+                exec('/opt/vc/bin/tvservice --preferred && chvt 6 && chvt 7', null);
+            }
+
+            // Setup for alwaysOn switch
+            if (this.config.alwaysOnPin) {
+                this.alwaysOn = new Gpio(this.config.alwaysOnPin, 'in', 'both');
+                this.alwaysOn.watch(function (err, value) {
+                    if (value === this.config.alwaysOnState) {
+                        self.sendSocketNotification('ALWAYS_ON', true);
+                        self.sendNotification('SHOW_ALERT', {
+                            title: 'Always-On Activated',
+                            message: 'Mirror will not activate power-saving mode'
+                        });
+                        if (self.config.powerSaving){
+                            clearTimeout(self.deactivateMonitorTimeout);
+                        }
+                    } else if (value === (this.config.alwaysOnState + 1) % 2) {
+                        self.sendSocketNotification('ALWAYS_ON', false);
+                        self.sendNotification('SHOW_ALERT', {
+                            title: 'Always-On Deactivated',
+                            message: 'Mirror will now use motion sensor to activate'
+                        });
+                    }
+                })
+            }
+
+            // Setup for alwaysOff switch
+            if (this.config.alwaysOffPin) {
+                this.alwaysOff = new Gpio(this.config.alwaysOffPin, 'in', 'both');
+                this.alwaysOff.watch(function (err, value) {
+                    if (value === this.config.alwaysOffState) {
+                        self.sendSocketNotification('ALWAYS_OFF', true);
+                        self.deactivateMonitor();
+                    } else if (value === (this.config.alwaysOffState + 1) % 2) {
+                        self.sendSocketNotification('ALWAYS_OFF', false);
+                        self.activateMonitor();
+                        if (self.config.powerSaving){
+                            clearTimeout(self.deactivateMonitorTimeout);
+                        }
+                    }
+                })
+            }
+
+            // Setup for sensor pin
+            this.pir = new Gpio(this.config.sensorPin, 'in', 'both');
+
+            // Setup value which represent on and off
+            const valueOn = this.config.sensorState;
+            const valueOff = (this.config.sensorState + 1) % 2;
+
+            // Detected movement
+            this.pir.watch(function (err, value) {
+                if (value == valueOn) {
+                    self.sendSocketNotification('USER_PRESENCE', true);
+                    if (self.config.powerSaving){
+                        clearTimeout(self.deactivateMonitorTimeout);
+                        self.activateMonitor();
+                    }
+                }
+                else if (value == valueOff) {
+                    self.sendSocketNotification('USER_PRESENCE', false);
+                    if (!self.config.powerSaving){
+                        return;
+                    }
+
+                    self.deactivateMonitorTimeout = setTimeout(function() {
+                        self.deactivateMonitor();
+                    }, self.config.powerSavingDelay * 1000);
+                }
+            });
+
+            this.started = true;
+
+        } else if (notification === 'SCREEN_WAKEUP') {
+            this.activateMonitor();
+        }
     }
-  }
 
 });
